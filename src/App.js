2bd9c9ea7bc7d002a323c53408dc0f29d0aa6c38
Norbert Kovács <k.n.89@outlook.hu>
2023-04-17 13:49:14 +0200

    Mapping throw data.json

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import './App.scss'
 
+//Importing data.json
+import Data from './utils/data.json'
+import { useState } from 'react'
+
 function App() {
   return (
     <div className='App'>
       <div className='container-border-white'>
         <div className='container-actual'>
-          <div className='container-title'>
-            <h1 className='title'>Title</h1>
-          </div>
+          {Data.map((actualData, index) => {
+            return (
+              <div key={index}>
+                <div className='container-title'>
+                  <h1 className='title'>{actualData.name}</h1>
+                </div>
 
-          <div className='container-date'>
-            <p className='date'>Date</p>
-          </div>
+                <div className='container-date'>
+                  <p className='date'>
+                    {actualData.from} - {actualData.to}
+                  </p>
+                </div>
 
-          <div className='container-members'>
-            <h2 className='member'>member</h2>
-            <h2 className='member'>member</h2>
-            <h2 className='member'>member</h2>
-          </div>
+                <div className='container-members'>
+                  <h2>{actualData.talent}</h2>
+                </div>
+              </div>
+            )
+          })}
 
           <button>Toggle</button>
         </div>
