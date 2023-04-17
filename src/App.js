33483c8fe8045b99372650ee0b176095d62c8fa4
Norbert Kovács <k.n.89@outlook.hu>
2023-04-17 22:50:02 +0200

    Toggle function finished + styling

--- src/App.js
+++ src/App.js
@@ -5,14 +5,45 @@ import Data from './utils/data.json'
 import { useState } from 'react'
 
 function App() {
+  const [indexes, setIndexes] = useState(0)
+
+  const toggleClass = () => {
+    let wrapper = document.querySelector('.wrapper')
+
+    const removePrevClass = () => {
+      wrapper.childNodes[indexes - 1].classList.remove('show')
+      wrapper.childNodes[indexes].classList.add('show')
+    }
+
+    const removeLastClass = () => {
+      wrapper.childNodes[indexes].classList.remove('show')
+      setIndexes(0)
+    }
+
+    const addClass = () => {
+      wrapper.childNodes[indexes].classList.add('show')
+      setIndexes(indexes + 1)
+    }
+
+    indexes >= 1 ? removePrevClass() : addClass()
+
+    indexes < 2 ? addClass() : removeLastClass()
+
+    indexes === 2
+      ? wrapper.childNodes[indexes].classList.add('show')
+      : wrapper.childNodes[2].classList.remove('show')
+
+    console.log(indexes)
+  }
+
   return (
     <div className='App'>
       <div className='container-border-white'>
-        <div className='container-actual'>
+        <div className='wrapper'>
           {Data.map((actualData, index) => {
             return (
-              <div key={index}>
-                <div className='container-title'>
+              <div className='container-actual' key={index}>
+                <div className='container-title' style={{ backgroundColor: actualData.color }}>
                   <h1 className='title'>{actualData.name}</h1>
                 </div>
 
@@ -22,15 +53,16 @@ function App() {
                   </p>
                 </div>
 
-                <div className='container-members'>
-                  <h2>{actualData.talent}</h2>
+                <div className='container-members' style={{ backgroundColor: actualData.color }}>
+                  <h2 className='members'>{actualData.talent[0]}</h2>
+                  <h2 className='members'>{actualData.talent[1]}</h2>
+                  <h2 className='members'>{actualData.talent[2]}</h2>
                 </div>
               </div>
             )
           })}
-
-          <button>Toggle</button>
         </div>
+        <button onClick={toggleClass}>Toggle</button>
       </div>
     </div>
   )
