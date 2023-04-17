f7e81eee270883f12de4c0ab0d9293736920dcdd
Norbert Kovács <k.n.89@outlook.hu>
2023-04-17 13:13:01 +0200

    Building the base structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import './App.css'
+import './App.scss'
 
 function App() {
-  return <div className='App'></div>
+  return (
+    <div className='App'>
+      <div className='container-border-white'>
+        <div className='container-actual'>
+          <div className='container-title'>
+            <h1 className='title'>Title</h1>
+          </div>
+
+          <div className='container-date'>
+            <p className='date'>Date</p>
+          </div>
+
+          <div className='container-members'>
+            <h2 className='member'>member</h2>
+            <h2 className='member'>member</h2>
+            <h2 className='member'>member</h2>
+          </div>
+
+          <button>Toggle</button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App
